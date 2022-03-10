46610c75c75113a940a023ca0b48ed82eb8f5e7b
Leonardo Mazza <desenvolvedormazza@gmail.com>
2022-03-10 14:59:38 -0300

    ventures hero e sobre

---
+++ wp-content/themes/tivit/template-pages/ventures.php
@@ -0,0 +1,48 @@
+<?php /* Template Name: Ventures */ ?>
+
+<?php get_header(); ?>
+
+<!-- Hero -->
+
+<div class="ventures-hero">
+
+  <div class="ventures-hero-title">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <h1>TIVIT VENTURES</h1>
+          <h2 class="hide-mobile">Conheça a nossa cultura empreendedora</h2>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="ventures-hero-content hide-desktop">
+    <h2>Nome do cliente videocase Lorem ipsum</h2>
+  </div>
+
+  <div class="botao hide-desktop">
+    <a href="#" class="btn btn-primary">Assista ao Reel</a>
+  </div>
+
+  <div class="arrow text-center hide-desktop">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/home/icons/seta-red.svg" class="bounce" alt="Saiba Mais">
+  </div>
+  
+</div>
+
+
+<!-- Sobre -->
+<div class="a-tivit-sobre ventures">
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <p>
+        A TIVIT anunciou recentemente a criação do <span class="red">TIVIT Ventures</span>, vertical de investimentos e aquisições criada para encontrar empresas que fortaleçam o portfólio de serviços oferecidos pela marca.
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php get_footer(); ?>
