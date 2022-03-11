5c2e59542c35a9f6a1c74536b89d178355aa5a35
Leonardo Mazza <desenvolvedormazza@gmail.com>
2022-03-11 14:56:13 -0300

    Seção Como Investimos

--- wp-content/themes/tivit/template-pages/ventures.php
+++ wp-content/themes/tivit/template-pages/ventures.php
@@ -45,4 +45,79 @@
   </div>
 </div>
 
+
+<!-- TDX Ventures -->
+<div id="tdx" class="tdx ventures">
+  <div class="container">
+    <div class="title w-100 text-center m-auto">
+      <h2>Como investimos?</h2>
+    </div>
+    <div class="row mt-2">
+      <div class="col-sm-12 col-md-3">
+        <div class="box">
+          <div class="w-100">
+            <div class="w-100 m-auto text-center">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/ventures/empreendedorismo.svg" alt="empreendedorismo">
+            </div>
+            <h2>
+              empreendedorismo
+            </h2>
+            <p>
+              As ventures aceleram o crescimento e reforçam a cultura empreendedora da TIVIT.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-12 col-md-3">
+        <div class="box">
+          <div class="w-100">
+            <div class="w-100 m-auto text-center">
+              <img src="<?php echo get_template_directory_uri(); ?>/assets/images/ventures/ideia-icon.svg" alt="ideia">
+            </div>
+            <h2>
+              inovação
+            </h2>
+            <p>
+              Além dos projetos orgânicos internos, buscamos novas soluções inovadoras no ecossistema de startups.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-12 col-md-3">
+        <div class="box">
+          <div class="w-100">
+            <div class="w-100 m-auto text-center">
+              <img src="<?php echo get_template_directory_uri(); ?>/assets/images/inovacao/icocode.svg" alt="code">
+            </div>
+            <h2>
+              crescimento
+            </h2>
+            <p>
+              O objetivo é adquirir startups de modelo SaaS com trajetórias de forte crescimento e escalabilidade.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-12 col-md-3">
+        <div class="box">
+          <div class="w-100">
+            <div class="w-100 m-auto text-center">
+              <img src="<?php echo get_template_directory_uri(); ?>/assets/images/ventures/estrelas-icon.svg" alt="grow">
+            </div>
+            <h2>
+              autonomia
+            </h2>
+            <p>
+              As startups têm todo o suporte da TIVIT, ao mesmo tempo que mantém sua autonomia de gestão.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="cta w-100 d-flex align-items-center justify-content-center">
+      <a class="button">APLICAÇÃO DE EMPRESAS</a>
+    </div>
+  </div>
+</div>
+
 <?php get_footer(); ?>
