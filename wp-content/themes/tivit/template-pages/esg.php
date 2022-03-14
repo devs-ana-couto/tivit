a191d7a11bee292f5b78c55ace4781f18d3ee9d7
Augusto M <gukis2189@hotmail.com>
2022-03-14 19:39:32 -0300

    Update on esg

--- wp-content/themes/tivit/template-pages/esg.php
+++ wp-content/themes/tivit/template-pages/esg.php
@@ -1,4 +1,199 @@
 <?php /* Template Name: ESG */ ?>
 <?php get_header(); ?>
 
+<!-- Hero banner-->
+<div class="esg-hero position-relative">
+  <div class="esg-hero-title">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+            <div class="zindex position-relative">
+                <h1>ESG</h1>
+                <h2 class="">ESG: um compromisso TIVIT.</h2>
+            </div>
+            <div class="d-flex justify-content-center align-items-center">
+                <img class="mask w-100 position-absolute" src="<?php echo get_template_directory_uri(); ?>/assets/images/esg/v_slider_home.svg" alt="mask" />
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div id="esgCounter" class="lp-tbanks-conheca">
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+
+        <div class="lp-tbanks-credibilidade text-center">
+          <div class="row">
+            <div class="col-12 col-md-3">
+              <p>
+                <span>
+                  <b>+30</b>
+                </span>
+              </p>
+              <div class="w-100 d-flex justify-content-center align-items-center">
+                  <p class="custom-text">
+                    Projetos apoiados
+                  </p>
+              </div>
+            </div>
+
+            <div class="col-12 col-md-3">
+              <p>
+                <span>+900</span>
+              </p>
+              <div class="w-100 d-flex justify-content-center align-items-center">
+                  <p class="custom-text">
+                    Empregos gerados
+                  </p>
+              </div>
+            </div>
+
+            <div class="col-12 col-md-3">
+              <p>
+                <span>+600</span><small>MIL</small>
+              </p>
+              <div class="w-100 d-flex justify-content-center align-items-center">
+                  <p class="custom-text">
+                    Pessoas impactadas pelos projetos
+                  </p>
+              </div>
+            </div>  
+
+            <div class="col-12 col-md-3">
+              <p>
+                <span>+300</span>
+              </p>
+              <div class="w-100 d-flex justify-content-center align-items-center">
+                  <p class="custom-text">
+                  Empregos gerados somente na pandemia
+                  </p>
+              </div>
+            </div> 
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</div>
+
+<div id="esgConheca" class="lp-tbanks-conheca">
+    <div class="container">
+        <div class="lp-tbanks-arrow text-center mb-5">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/landing/tbanks/icons/seta-red.svg" class="bounce" alt="Tbanks Saiba Mais">
+        </div>
+        <div class="row">
+            <div class="col-12">
+
+                <div class="lp-tbanks-conheca-content">
+                    <p>
+                    Uma frase sobre os números dos projetos ESGs lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Bloco de Conteúdos -->
+<div class="home-content content-inovacao">
+<div id="triangle-down"></div>
+  <div class="container pd">
+    <div class="title">
+      <h2 class="titleText text-center">NOSSOS CONTEÚDOS</h2>
+    </div>
+    <div class="row">
+      <!-- content 1 -->
+      <div class="col-12 col-md-4">
+        <div class="cardContent p-1"> 
+          <div class="img">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/home/artigo1.png" alt="Depoimento">
+          </div>
+          <div class="detalhes">
+            <span>01/01/2021</span>
+            <p class="m-0 h-100">Por <b>Ana Helena Lazaroni</b></p>
+          </div>
+          <div class="content">
+            <h3>título do artigo 1 Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
+          </div>
+          <div class="autor-time w-100">
+            <div class="d-flex flex-row">
+              <a href="#">conta híbrida</a>
+              <a href="#">varejo</a>
+            </div>
+            <p>6 minutos de leitura</p>
+          </div>
+          <div class="acessar">
+            <a href="#">acessar artigo</a>
+          </div>
+        </div>
+      </div>
+      <!-- // content 1 -->
+
+      <!-- content 2 -->
+      <div class="col-12 col-md-4">
+        <div class="cardContent p-1">
+          <div class="title">
+            <h2 class="hide-desktop">cases mais recentes</h2>
+          </div>
+          <div class="img">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/home/artigo2.png" alt="Depoimento">
+          </div>
+          <div class="detalhes">
+            <span>01/01/2021</span>
+            <p class="m-0 h-100">Por <b>Ana Helena Lazaroni</b></p>
+          </div>
+          <div class="content">
+            <h3>título do artigo 1 Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
+          </div>
+          <div class="autor-time w-100">
+            <div class="d-flex flex-row">
+              <a href="#">conta híbrida</a>
+              <a href="#">varejo</a>
+            </div>
+            <p>6 minutos de leitura</p>
+          </div>
+          <div class="acessar">
+            <a href="#">acessar artigo</a>
+          </div>
+        </div>
+      </div>
+      <!-- // content 2 -->
+      <!-- content 3 -->
+      <div class="col-12 col-md-4">
+        <div class="cardContent p-1">
+          <div class="title">
+            <h2 class="hide-desktop">cases mais recentes</h2>
+          </div>
+          <div class="img">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/home/artigo3.png" alt="Depoimento">
+          </div>
+          <div class="detalhes">
+            <span>01/01/2021</span>
+            <p class="m-0 h-100">Por <b>Ana Helena Lazaroni</b></p>
+          </div>
+          <div class="content">
+            <h3>título do artigo 1 Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
+          </div>
+          <div class="autor-time w-100">
+            <div class="d-flex flex-row align-items-center">
+              <a href="#">conta híbrida</a>
+              <a href="#">varejo</a>
+            </div>
+            <p>6 minutos de leitura</p>
+          </div>
+          <div class="acessar">
+            <a href="#">acessar artigo</a>
+          </div>
+        </div>
+      </div>
+      <!-- // content 3 -->
+    </div>
+  </div>
+</div>
+
 <?php get_footer(); ?>
