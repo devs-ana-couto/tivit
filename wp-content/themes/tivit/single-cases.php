94f49ae025e3f72f9d69134a53e18dea4560bd43
Leonardo Mazza <desenvolvedormazza@gmail.com>
2022-03-17 16:44:32 -0300

    Sobre o Cliente Mobile

--- wp-content/themes/tivit/single-cases.php
+++ wp-content/themes/tivit/single-cases.php
@@ -6,7 +6,6 @@ $detect = new Mobile_Detect;
 if(have_posts()) : while(have_posts()) : the_post();
 ?>
 
-
 <!-- Section Banner -->
 <section class="banner-cases-item">
     <div class="case-item-background" style="background: url('<? $detect->isMobile() ? the_field('banner_mobile') : the_field('banner_desktop'); ?>') no-repeat center center; background-size: cover;">
@@ -14,7 +13,7 @@ if(have_posts()) : while(have_posts()) : the_post();
             <div class="row">
                 <div class="col-12">
                     <div class="case-item-content">
-                        <h2>Cases</h2>
+                        <h2>Cliente: <strong><? the_field('nome_do_cliente'); ?></strong></h2>
                         <h1><?php the_title(); ?></h1>
                     </div>
                     <div class="case-item-logos">
@@ -33,6 +32,33 @@ if(have_posts()) : while(have_posts()) : the_post();
     </div>
 </section>
 
+<!-- Section Empresa -->
+<section class="empresa-cases-item">
+    <div class="container">
+        <div class="row">
+            <div class="col-8">
+                <h2>Sobre o cliente</h2>
+            </div>
+            <div class="col-4">
+                <img src="<? the_field('logo_da_empresa'); ?>" alt="">
+            </div>
+        </div>
+        <div class="row mt-4">
+            <div class="col-12">
+                <p><? the_field('descricao'); ?></p>
+            </div>
+        </div>
+        <div class="row mt-3">
+            <div class="col-12">
+                <a href="contato" class="btn-entre-em-contato">Entre em Contato</a>
+            </div>
+            <div class="col-12">
+                <a href="<? the_field('download'); ?>" class="btn-download">Download <img src="<?php echo get_template_directory_uri(); ?>/assets/images/custom-post-types/cases/save_alt.svg" alt="...">
+</a>
+            </div>
+        </div>
+    </div>
+</section>
 
 <?php
 endwhile; endif;
