ba712b5e5e9d6a857af774fe82cd304151d6ceff
Augusto M <gukis2189@hotmail.com>
2022-03-22 16:06:25 -0300

    Tdx mobile finished

--- wp-content/themes/tivit/assets/js/tivit.js
+++ wp-content/themes/tivit/assets/js/tivit.js
@@ -329,6 +329,22 @@ document.addEventListener("DOMContentLoaded", function(){
             next = next.nextElementSibling
          }
       })
+
+      let items3 = document.querySelectorAll('.carousel .content')
+
+      items3.forEach((el) => {
+         const minPerSlide = 2
+         let next = el.nextElementSibling
+         for (var i=1; i<minPerSlide; i++) {
+            if (!next) {
+                  // wrap carousel by using first child
+               next = items3[0]
+               }
+            let cloneChild = next.cloneNode(true)
+            el.appendChild(cloneChild.children[0])
+            next = next.nextElementSibling
+         }
+      })
    }
 
    if(window.location.pathname === '/staged/tivit/esg' || window.location.pathname === '/staged/tivit/esg/'){
--- wp-content/themes/tivit/template-pages/tdx.php
+++ wp-content/themes/tivit/template-pages/tdx.php
@@ -123,11 +123,11 @@
 
         <div class="carousel-inner">
         
-          <div class="row">
+          <div class="row columnReverseMobile">
             <div class="col-12 col-md-6 lp-tbanks-confira-bg-right lp-tdx-confira-bg-right">
               <div class="lp-tbanks-confira-content">
                 <h3>
-                Cases de <br> sucesso da <strong>Tdx</strong>
+                Cases de sucesso <br> da <strong>TDX</strong>
                 </h3>
               </div>
             </div>
@@ -143,8 +143,18 @@
                     </p>
                   </div>
                 </div>
-                <!-- <div class="col-12 lp-tbanks-confira-bg-mobile hide-desktop">
-                </div> -->
+                <div class="col-12 lp-tbanks-confira-bg-mobile hide-desktop">
+                  <div class="position-relative">
+                    <img class="w-100" src="<?php echo get_template_directory_uri(); ?>/assets/images/landing/tdx/mobileslide1.svg" alt="Mobile Slide 1">
+                    <div class="lp-tbanks-confira-carousel d-flex flex-column justify-content-center h-100 position-absolute">
+                      <h2>cnh industrial</h2>
+                      <p class="paragrafo3">
+                        Plataforma digital mobile desenvolvida de ponta a ponta, com serviços para concessionárias: simulações de crédito, captação, envio e aprovação de documentos.
+                      </p>
+                      <a>CONHEÇA A AÇÃO</a>
+                    </div>
+                  </div>
+                </div>
               </div>
 
               <div class="carousel-item heroslide">
@@ -156,8 +166,18 @@
                     </p>
                   </div>
                 </div>
-                <!-- <div class="col-12 lp-tbanks-confira-bg-mobile hide-desktop">
-                </div> -->
+                <div class="col-12 lp-tbanks-confira-bg-mobile hide-desktop">
+                  <div class="position-relative">
+                    <img class="w-100" src="<?php echo get_template_directory_uri(); ?>/assets/images/landing/tdx/mobileslide1.svg" alt="Mobile Slide 1">
+                    <div class="lp-tbanks-confira-carousel d-flex flex-column justify-content-center h-100 position-absolute">
+                      <h2>cnh industrial</h2>
+                      <p class="paragrafo3">
+                        Plataforma digital mobile desenvolvida de ponta a ponta, com serviços para concessionárias: simulações de crédito, captação, envio e aprovação de documentos.
+                      </p>
+                      <a>CONHEÇA A AÇÃO</a>
+                    </div>
+                  </div>
+                </div>
               </div>
 
               <div class="carousel-item heroslide">
@@ -169,8 +189,18 @@
                     </p>
                   </div>
                 </div>
-                <!-- <div class="col-12 lp-tbanks-confira-bg-mobile hide-desktop">
-                </div> -->
+                <div class="col-12 lp-tbanks-confira-bg-mobile hide-desktop">
+                  <div class="position-relative">
+                    <img class="w-100" src="<?php echo get_template_directory_uri(); ?>/assets/images/landing/tdx/mobileslide1.svg" alt="Mobile Slide 1">
+                    <div class="lp-tbanks-confira-carousel d-flex flex-column justify-content-center h-100 position-absolute">
+                      <h2>cnh industrial</h2>
+                      <p class="paragrafo3">
+                        Plataforma digital mobile desenvolvida de ponta a ponta, com serviços para concessionárias: simulações de crédito, captação, envio e aprovação de documentos.
+                      </p>
+                      <a>CONHEÇA A AÇÃO</a>
+                    </div>
+                  </div>
+                </div>
 
               </div>
 
@@ -317,7 +347,7 @@
               <div class="carousel-item heroslide4 values active">
                   <div class="col-11 m-0 p-0">
                     <div class=" card">
-                      <img class="fade-in-bottom" src="<?php echo get_template_directory_uri(); ?>/assets/images/landing/tdx/icons/pc-icon.svg" alt="Tdx powered by TIVIT">
+                      <img class="" src="<?php echo get_template_directory_uri(); ?>/assets/images/landing/tdx/icons/pc-icon.svg" alt="Tdx powered by TIVIT">
                       <h3>Design, Produtos & Plataformas</h3>
                       <div class="d-flex justify-content-center align-items-center w-100">
                       <p class="paragrafo3 mx">Soluções digitais inovadoras na prática: desenvolvemos plataformas completas, que priorizam a experiência do usuário (UX) e a potencialização dos resultados.</p>
@@ -336,7 +366,7 @@
               <div class="carousel-item heroslide4 values">
                   <div class="col-11 m-0 p-0">
                     <div class=" card">
-                      <img class="fade-in-bottom" src="<?php echo get_template_directory_uri(); ?>/assets/images/landing/tdx/icons/grafico2-icon.svg" alt="Tdx powered by TIVIT">
+                      <img class="" src="<?php echo get_template_directory_uri(); ?>/assets/images/landing/tdx/icons/grafico2-icon.svg" alt="Tdx powered by TIVIT">
                       <h3>Data & Analytics</h3>
                       <div class="d-flex justify-content-center align-items-center w-100">
                         <p class="paragrafo3 mx">Tecnologias, referências e práticas direcionadas para liberar todo o potencial dos dados e gerar relevância para negócios extraordinários.</p>
@@ -355,7 +385,7 @@
               <div class="carousel-item heroslide4 values">
                   <div class="col-11 m-0 p-0">
                     <div class=" card">
-                      <img class="fade-in-bottom" src="<?php echo get_template_directory_uri(); ?>/assets/images/landing/tdx/icons/codigo-icon.svg" alt="Tdx powered by TIVIT">
+                      <img class="" src="<?php echo get_template_directory_uri(); ?>/assets/images/landing/tdx/icons/codigo-icon.svg" alt="Tdx powered by TIVIT">
                       <h3>Modernização & Otimização</h3>
                       <div class="d-flex justify-content-center align-items-center w-100">
                         <p class="paragrafo3 mx">Digitalização e transformação de aplicações legadas e ambientes ultrapassados para habilitar a escalada sustentável do negócio.</p>
@@ -374,7 +404,7 @@
               <div class="carousel-item heroslide4 values">
                   <div class="col-11 m-0 p-0">
                     <div class=" card">
-                      <img class="fade-in-bottom" src="<?php echo get_template_directory_uri(); ?>/assets/images/landing/tdx/icons/grafico-icon.svg" alt="Tdx powered by TIVIT">
+                      <img class="" src="<?php echo get_template_directory_uri(); ?>/assets/images/landing/tdx/icons/grafico-icon.svg" alt="Tdx powered by TIVIT">
                       <h3>Sustentação & Crescimento</h3>
                       <div class="d-flex justify-content-center align-items-center w-100">
                         <p class="paragrafo3 mx">Serviços e plataformas que garantem clareza e segurança para cenários e ambientes complexos.</p>
@@ -561,13 +591,14 @@
 </div>
 
 <!-- Bloco de Conteúdos -->
-<div class="home-content content-inovacao">
+<div id="contentTdx" class="home-content content-inovacao">
 <div id="triangle-down"></div>
   <div class="container pd">
     <div class="title">
       <h2 class="titleText text-center">NOSSOS CONTEÚDOS</h2>
     </div>
-    <div class="row">
+
+    <div class="row hide-mobile">
       <!-- content 1 -->
       <div class="col-12 col-md-4">
         <div class="cardContent p-1"> 
@@ -657,6 +688,96 @@
       </div>
       <!-- // content 3 -->
     </div>
+
+    <!-- mobile slide -->
+    <div class="row mx-auto my-auto justify-content-center hide-desktop">
+      <div id="contentMobileCarousel" class="carousel slide p-0" data-bs-ride="carousel">
+          <div class="carousel-inner" role="listbox">
+              <div class="carousel-item heroslide3 content active">
+                  <div class="col-11 m-0 p-0">
+                    <div class="cardContent p-2"> 
+                      <div class="img position-relative">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/home/artigo1.png" alt="Depoimento">
+                        <div class="position-absolute tagContent">artigo</div>
+                      </div>
+                      <div class="detalhes">
+                        <span>01/01/2021</span>
+                        <p class="m-0 h-100">Por <b>Ana Helena Lazaroni</b></p>
+                      </div>
+                      <div class="content">
+                        <h3>título do artigo 1 Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
+                      </div>
+                      <div class="autor-time w-100">
+                        <div class="d-flex flex-row aaa">
+                          <a href="#">conta híbrida</a>
+                          <a href="#">varejo</a>
+                        </div>
+                        <p>6 min. de leitura</p>
+                      </div>
+                      <div class="acessar">
+                        <a href="#">acessar artigo</a>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+              <div class="carousel-item heroslide3 content">
+                  <div class="col-11 m-0 p-0">
+                    <div class="cardContent p-2">
+                      <div class="img position-relative">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/home/artigo2.png" alt="Depoimento">
+                        <div class="position-absolute tagContent">press release</div>
+                      </div>
+                      <div class="detalhes">
+                        <span>01/01/2021</span>
+                        <p class="m-0 h-100">Por <b>Ana Helena Lazaroni</b></p>
+                      </div>
+                      <div class="content">
+                        <h3>título do artigo 1 Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
+                      </div>
+                      <div class="autor-time w-100">
+                        <div class="d-flex flex-row">
+                          <a href="#">conta híbrida</a>
+                          <a href="#">varejo</a>
+                        </div>
+                        <p>6 min. de leitura</p>
+                      </div>
+                      <div class="acessar">
+                        <a href="#">acessar artigo</a>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+              <div class="carousel-item heroslide3 content">
+                  <div class="col-11 m-0 p-0">
+                    <div class="cardContent p-2">
+                      <div class="img position-relative">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/home/artigo3.png" alt="Depoimento">
+                        <div class="position-absolute tagContent">artigo</div>
+                      </div>
+                      <div class="detalhes">
+                        <span>01/01/2021</span>
+                        <p class="m-0 h-100">Por <b>Ana Helena Lazaroni</b></p>
+                      </div>
+                      <div class="content">
+                        <h3>título do artigo 1 Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
+                      </div>
+                      <div class="autor-time w-100">
+                        <div class="d-flex flex-row align-items-center">
+                          <a href="#">conta híbrida</a>
+                          <a href="#">varejo</a>
+                        </div>
+                        <p>6 min. de leitura</p>
+                      </div>
+                      <div class="acessar">
+                        <a href="#">acessar artigo</a>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+    </div>
+
   </div>
 </div>
 
