1d6d7113baf940db7fa7e8660ff9be92696244d3
Leonardo Mazza <desenvolvedormazza@gmail.com>
2022-03-23 16:09:59 -0300

    Links Externos

--- wp-content/themes/tivit/single-cases.php
+++ wp-content/themes/tivit/single-cases.php
@@ -299,11 +299,20 @@ if(have_posts()) : while(have_posts()) : the_post();
 </section>
 
 <!-- Section Links Externos -->
+
 <section class="links-externos-item-case">
     <div class="container">
-        <div class="row">
         <?php
-        // Check rows exists.   
+            if($detect->isMobile()){
+                echo '<div class="row mx-auto my-auto justify-content-center">
+                        <div id="paymentMobileCarousel" class="carousel slide p-0" data-bs-ride="carousel">
+                            <div class="carousel-inner" role="listbox">';
+
+            }else{
+                echo '<div class="row">';
+            }
+
+            // Check rows exists.   
             if( have_rows('links_externos') ):
                 // Loop through rows.
                 for($i=0; have_rows('links_externos'); $i++) : the_row();
@@ -312,29 +321,60 @@ if(have_posts()) : while(have_posts()) : the_post();
                     $link_description[$i] = get_sub_field('descricao');
                     $link_url[$i] = get_sub_field('url_do_link');
             ?>
-            <div class="col-12 col-md-4">
+        
+            <!-- Desktop -->
+            <div class="col-12 col-md-4 hide-mobile">
                 <div class="box">
-                    <div class="category">                    
-                        <span><?=$link_category[$i];?></span>
+                    <div class="category">
+                        <span><?=$link_category[$i]?></span>
                     </div>
                     <div class="title">
-                        <h3><?=$link_title[$i];?></h3>
+                        <h3><?=$link_title[$i]?></h3>
                     </div>
                     <div class="description">
-                        <p><?=$link_description[$i];?></p>
+                        <p><?=$link_description[$i]?></p>
                     </div>
                     <div class="button">
-                        <a href="<?=$link_url[$i];?>" class="btn btn-primary">Saiba mais</a>
+                        <a href="<?=$link_url[$i]?>" target="_blank" class="btn btn-primary">Leia mais</a>
                     </div>
-                    
                 </div>
             </div>
+            
+            <!-- Mobile slide -->
+            <div class="carousel-item col-md-4 hide-desktop heroslide4 payment <?=$i == 0 ? 'active' : '';?>">
+                <div class="col-11 m-0 p-0">
+                    <div class="box">
+                        <div class="category">
+                            <span><?=$link_category[$i]?></span>
+                        </div>
+                        <div class="title">
+                            <h3><?=$link_title[$i]?></h3>
+                        </div>
+                        <div class="description">
+                            <p><?=$link_description[$i]?></p>
+                        </div>
+                        <div class="button">
+                            <a href="<?=$link_url[$i]?>" target="_blank" class="btn btn-primary">Leia mais</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+                    
             <?php
-            // End loop.
-            endfor;
-            endif;
+                // End loop.
+                endfor;
+                endif;
+            
+            if($detect->isMobile()){
+                echo '</div>
+                    </div>
+                </div>';
+            }else{
+                echo '</div>';
+            }
         ?>
-        </div>
+        
+            
     </div>
 </section>
 
