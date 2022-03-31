332ab91cfc5a374e31a6192d87fdf1a76cc3dec5
Augusto M <gukis2189@hotmail.com>
2022-03-31 15:41:29 -0300

    Solucoes

--- wp-content/themes/tivit/template-pages/solucoes.php
+++ wp-content/themes/tivit/template-pages/solucoes.php
@@ -26,7 +26,53 @@
     <img src="<?php echo get_template_directory_uri(); ?>/assets/images/contato/banner-mask.svg" class="position-absolute mask" alt="Máscara">
   </div>
 
+  <!-- only for this page DESKTOP -->
+  <div class="related hide-mobile">
+    <div class="row w-100">
+      <div class="col-6">
+        <div class="d-flex flex-row align-items-center justify-content-center w-100">
+          <p>Ofertado por:</p>
+          <span>TDX</span>
+          <span>STONE AGE</span>
+        </div>
+      </div>
+      <div class="col-6">
+        <div class="d-flex flex-row align-items-center justify-content-center w-100">
+          <p>Ofertas relacionadas:</p>
+          <span>DIGITAL PAYMENTS</span>
+          <span>DIGITAL BUSINESS</span>
+          <span>CYBERSECURITY</span>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- /// only for his page DESKTOP -->
 </div>
+
+<!-- only for this page MOBILE -->
+<div class="related hide-desktop">
+  <div class="row">
+    <div class="col-12">
+      <div class="d-flex flex-column align-items-center justify-content-center w-100">
+        <p>Ofertado por:</p>
+      </div>
+      <div class="d-flex flex-row align-items-center justify-content-center w-100">
+        <span>TDX</span>
+        <span>STONE AGE</span>
+      </div>
+    </div>
+    <div class="col-12 mt-3">
+      <div class="d-flex flex-column align-items-center justify-content-center w-100">
+        <p>Ofertas relacionadas:</p>
+      </div>
+      <div class="d-flex flex-row align-items-center justify-content-center w-100">
+        <span>DIGITAL PAYMENTS</span>
+        <span>DIGITAL BUSINESS</span>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- /// only for his page MOBILE -->
 
 <div id="services-intro">
     <div class="container d-flex justify-content-center align-items-center flex-column w-100">
@@ -265,11 +311,162 @@
                 </div>
             </div>
         </div>
+
+        <div class="publiccloud position-relative">
+        <div id="triangle-down"></div>
+        <div>
+          <img class="w-100 mask hide-mobile" src="<?php echo get_template_directory_uri(); ?>/assets/images/solucoes/mask.svg" alt="Mask" />
+          <img class="w-100 mask hide-desktop" src="<?php echo get_template_directory_uri(); ?>/assets/images/solucoes/maskmobile.svg" alt="Mask" />
+        </div>
+          <div class="mainImage text-center m-auto">
+            <img class="w-100 hide-mobile" src="<?php echo get_template_directory_uri(); ?>/assets/images/solucoes/publiccloud.jpg" alt="Public Cloud" />
+            <img class="w-100 hide-desktop" src="<?php echo get_template_directory_uri(); ?>/assets/images/solucoes/publiccloudmobile.jpg" alt="Public Cloud" />
+          </div>
+          <div class="customCloudPosition position-absolute w-100">
+            <div class="row custom-direction">
+              <div class="col-12 col-md-6 d-flex justify-content-end">
+                <div class="h-100">
+                  <h2>PUBLIC CLOUD</h2>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+              </div>
+              <div class="col-12 col-md-6">
+                <div>
+                  <img class="w-auto" src="<?php echo get_template_directory_uri(); ?>/assets/images/solucoes/play.svg" alt="Play" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
     </div>
 </div>
 
+<div id="strategy">
+  <div class="container">
+    <div class="intro">
+      <h2>O ponto de partida de nossos projetos inclui o levantamento do ambiente, entendimento do negócio, identificação de riscos e avaliação de viabilidade técnica e financeira.</h2>
+      <p>Nesta etapa de assessment, definimos, validamos e planejamos a estratégia adequada ao seu negócio, seja ela: </p>
+    </div>
+    <div class="row cards hide-mobile">
+      <div class="col-12 col-md-4">
+        <div class="intro text-center">
+          <h3>MIGRAÇÃO</h3>
+        </div>
+        <div class="card w-100">
+          <p>Criamos e implementamos as <b>estratégias de negócio</b> e as tecnologias necessárias, com equipes especializadas e práticas comprovadas, para garantir uma migração para a nuvem segura e eficaz.</p>
+        </div>
+        <div class="card w-100">
+          <p>Aplicamos os pilares do Well-Architected: <b>Excelência Operacional, Segurança, Confiabilidade, Performance e Custos.</b></p>
+        </div>
+        <div class="card w-100">
+          <p>Seguimos o guia de implantação <b>T-CAF</b> (TIVIT Cloud Adoption Framework) orientado pelas diretrizes de cada parceiro.</p>
+        </div>
+      </div>
+      <div class="col-12 col-md-4">
+        <div class="intro text-center">
+          <h3>TRANSFORMAÇÃO</h3>
+        </div>
+        <div class="card w-100">
+          <p>Transforme seu negócio com arquiteturas <b>“cloud native”</b> e modernização de aplicações.</p>
+        </div>
+        <div class="card w-100">
+          <p>Implantamos processos de disaster recovery em nuvem para construir soluções confiáveis. Mesmo diante de uma falha na infraestrutura de TI, o DRaaS possibilita que os sistemas <b>prossigam operando sem interrupções.</b></p>
+        </div>
+        <div class="card w-100">
+          <p>Também implementamos <b>soluções de DaaS</b> em nuvem pública, entregando de forma rápida e sem complicação os benefícios de uma plataforma gerenciada de estações de trabalho e aplicações virtualizadas.</p>
+        </div>
+        <div class="card w-100">
+          <p>Realizamos projetos de <b>migração e transformação de banco de dados</b> para otimizar o uso de recursos e o licenciamento em plataformas de BD em nuvem.</p>
+        </div>
+      </div>
+      <div class="col-12 col-md-4">
+        <div class="intro text-center">
+          <h3>OTIMIZAÇÃO</h3>
+        </div>
+        <div class="card w-100">
+          <p>Aumentamos o <b>valor de sua nuvem,</b> otimizando o uso de recursos, eliminando desafios e avaliando cenários de contratação.</p>
+        </div>
+        <div class="card w-100">
+          <p>No projeto de <b>curta-duração</b> (mínimo um mês), aplicamos as melhores práticas de gerenciamento financeiro em nuvem.</p>
+        </div>
+        <div class="card w-100">
+          <p>Assuma o <b>controle de seus gastos</b> e direcione os investimentos em TI de forma adequada.</p>
+        </div>
+      </div>
+    </div>
+
+    <!-- Mobile accordion -->
+    <div class="accordion hide-desktop" id="accordionExample">
+      <div class="accordion-item">
+        <h2 class="accordion-header" id="headingOne">
+          <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+            MIGRAÇÃO
+          </button>
+        </h2>
+        <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+          <div class="accordion-body">
+            <div class="card w-100">
+              <p>Criamos e implementamos as <b>estratégias de negócio</b> e as tecnologias necessárias, com equipes especializadas e práticas comprovadas, para garantir uma migração para a nuvem segura e eficaz.</p>
+            </div>
+            <div class="card w-100">
+              <p>Aplicamos os pilares do Well-Architected: <b>Excelência Operacional, Segurança, Confiabilidade, Performance e Custos.</b></p>
+            </div>
+            <div class="card w-100">
+              <p>Seguimos o guia de implantação <b>T-CAF</b> (TIVIT Cloud Adoption Framework) orientado pelas diretrizes de cada parceiro.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="accordion-item">
+        <h2 class="accordion-header" id="headingTwo">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+            TRANSFORMAÇÃO
+          </button>
+        </h2>
+        <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+          <div class="accordion-body">
+            <div class="card w-100">
+              <p>Transforme seu negócio com arquiteturas <b>“cloud native”</b> e modernização de aplicações.</p>
+            </div>
+            <div class="card w-100">
+              <p>Implantamos processos de disaster recovery em nuvem para construir soluções confiáveis. Mesmo diante de uma falha na infraestrutura de TI, o DRaaS possibilita que os sistemas <b>prossigam operando sem interrupções.</b></p>
+            </div>
+            <div class="card w-100">
+              <p>Também implementamos <b>soluções de DaaS</b> em nuvem pública, entregando de forma rápida e sem complicação os benefícios de uma plataforma gerenciada de estações de trabalho e aplicações virtualizadas.</p>
+            </div>
+            <div class="card w-100">
+              <p>Realizamos projetos de <b>migração e transformação de banco de dados</b> para otimizar o uso de recursos e o licenciamento em plataformas de BD em nuvem.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="accordion-item">
+        <h2 class="accordion-header" id="headingThree">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+            OTIMIZAÇÃO
+          </button>
+        </h2>
+        <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+          <div class="accordion-body">
+            <div class="card w-100">
+              <p>Aumentamos o <b>valor de sua nuvem,</b> otimizando o uso de recursos, eliminando desafios e avaliando cenários de contratação.</p>
+            </div>
+            <div class="card w-100">
+              <p>No projeto de <b>curta-duração</b> (mínimo um mês), aplicamos as melhores práticas de gerenciamento financeiro em nuvem.</p>
+            </div>
+            <div class="card w-100">
+              <p>Assuma o <b>controle de seus gastos</b> e direcione os investimentos em TI de forma adequada.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- //mobile accordion -->
+  </div>
+</div>
+
 <!-- Depoimentos -->
-<div id="pecDepoimentos" class="lp-tbanks-depoimentos">
+<div id="pecDepoimentos" class="lp-tbanks-depoimentos mx-custom">
   <div id="recipeCarousel" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-inner">
       <div class="carousel-item heroslide active">
