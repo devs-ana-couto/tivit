bf544949322b27c0496694c7466ccdd79a8b417c
Augusto M <gukis2189@hotmail.com>
2022-04-05 11:04:00 -0300

    Desdobramentos almost finished

--- wp-content/themes/tivit/assets/js/tivit.js
+++ wp-content/themes/tivit/assets/js/tivit.js
@@ -602,6 +602,38 @@ document.addEventListener("DOMContentLoaded", function(){
       })
    }
 
+   if(window.location.pathname === '/staged/tivit/desdobramento' || window.location.pathname === '/staged/tivit/desdobramento/'){
+      let items = document.querySelectorAll('.carousel .heroslide2')
+      items.forEach((el) => {
+         const minPerSlide = 6
+         let next = el.nextElementSibling
+         for (var i=1; i<minPerSlide; i++) {
+            if (!next) {
+                  // wrap carousel by using first child
+               next = items[0]
+               }
+            let cloneChild = next.cloneNode(true)
+            el.appendChild(cloneChild.children[0])
+            next = next.nextElementSibling
+         }
+      })
+
+      let items2 = document.querySelectorAll('.carousel .heroslide3')
+      items2.forEach((el) => {
+         const minPerSlide = 6
+         let next = el.nextElementSibling
+         for (var i=1; i<minPerSlide; i++) {
+            if (!next) {
+                  // wrap carousel by using first child
+               next = items2[0]
+               }
+            let cloneChild = next.cloneNode(true)
+            el.appendChild(cloneChild.children[0])
+            next = next.nextElementSibling
+         }
+      })
+   }
+
    if(window.location.pathname === '/staged/tivit/solucoes' || window.location.pathname === '/staged/tivit/solucoes/'){
       let items = document.querySelectorAll('.carousel .heroslide')
       items.forEach((el) => {
