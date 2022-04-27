a679997fb49f210984130ab2cad92fd3feadb16c
Leonardo Mazza <desenvolvedormazza@gmail.com>
2022-04-27 12:22:30 -0300

    Cor de Fundo Featured

--- wp-content/themes/tivit/components/cases/featured.php
+++ wp-content/themes/tivit/components/cases/featured.php
@@ -1,5 +1,5 @@
 <!-- Section Destaques -->
-<section class="destaques-cases-item">
+<section class="destaques-cases-item" style="background: <?=the_field('cor_de_fundo_exibir_featured_cases');?>">
     <div class="container">
         <div class="row">
             <div class="col-12 col-md-10 offset-md-1">
