6a3d9283f1a7fc78236924c0827393dd9f6a4660
Bruno Nogueira Martins <bn.martins02@gmail.com>
2022-07-07 01:04:48 -0300

    Modulo box-intro

---
+++ wp-content/themes/tivit/template-pages/modulos.php
@@ -0,0 +1,48 @@
+<?php /* Template Name: Modulos */ ?>
+<?php get_header(); ?>
+    <!-- remover a classe dev no back end -->
+    <div class="dev"></div>
+    <section class="container box-intro">
+        <!-- passar a cor adivinda do back para o inline abaixo, caso não tenha cor setada por padrão deverá ser a cor setada na abaixo, vale também para imagens -->
+        <div class="col-12 box-top" style="background: #454D54;">
+            <div class="row row-cols-2 h-100 justify-content-center align-items-center">
+                <!-- para facilitar na implementação do back, como os texot serão posicionado de acordo com
+                 a vontade do usuario passe a order1, order2, order3 ou order4 na class -->
+                <div class="col-12 col-lg order-1 d-flex justify-content-center align-items-center">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.svg"
+                         class="logo-tivit" alt="">
+                </div>
+                <div class="col-12 col-lg order-2 d-flex justify-content-center align-items-center">
+                    <p>8 entre as 10 maiores empresas da América Latina contam com a TIVIT como parceira
+                        tecnológica
+                        para trazer eficiência e inovação para os seus negócios.</p>
+                </div>
+            </div>
+        </div>
+        <!-- passar a cor adivinda do back para o inline abaixo, caso não tenha cor setada por padrão deverá ser a cor setada na abaixo, vale também para imagens -->
+        <div class="col-12 box-below"
+             style="background: url('<?php echo get_template_directory_uri(); ?>/assets/images/box-intro/background.png'); background-repeat: no-repeat; background-size: cover;">
+            <!-- caso passe uma imagem no inline acima, o card-img-overlay deverá ser inserido abaixo antes das col-auto -->
+            <!--           -->
+            <div class="row row-cols-2 h-100">
+                <!-- para facilitar na implementação do back, como os texot serão posicionado de acordo com
+                                       a vontade do usuario passe a order1, order2, order3 ou order4 na class -->
+                <div class="col-auto col-lg order-1 d-flex flex-wrap justify-content-center align-content-center">
+                    <h4>Seu negócio hoje age proativamente contra ataques ou apenas reage?</h4>
+                    <p>Prevenção é a chave para diminuir custos e manter a reputação da sua empresa.</p>
+                </div>
+                <div class="col-auto col-lg order-2 d-flex flex-wrap justify-content-center align-content-center">
+                    <div class="col-12 box-cta">
+                        <p>Entre em contato com nossos consultores e saiba como o serviços de Cyber Security podem
+                            mitigar
+                            os riscos e vulnerabilidades do seu negócio.</p>
+                        <a href="" class="btn btn-outline-primary">Fale com nossos consultores</a>
+                    </div>
+                </div>
+            </div>
+            <div class="card-img-overlay mask"></div>
+        </div>
+    </section>
+
+
+<?php get_footer(); ?>
