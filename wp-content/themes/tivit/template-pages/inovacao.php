df81d35606618a8a0477ebb82ba90732022c67df
Bruno Nogueira Martins <bn.martins02@gmail.com>
2022-09-20 01:42:36 -0300

    .

---
+++ wp-content/themes/tivit/template-pages/inovacao.php
@@ -0,0 +1,12 @@
+<?php /* Template Name: Inovação */ ?>
+<?php get_header(); ?>
+
+<?php page_bulder_init(get_the_ID(), $post, true); ?>
+
+    <!-- Bloco de Conteúdos -->
+    <!-- <div id="contentTdx" class="home-content content-inovacao"> -->
+        <!-- <div id="triangle-down"></div> -->
+        <?php // do_shortcode('[ac-bloco-conteudo]'); ?>
+    <!-- </div> -->
+    <!-- </div> -->
+<?php get_footer(); ?>
